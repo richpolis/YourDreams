d4790a780d2cc4b6a445b0cf23bb992806200c09
Ricardo Alcantara <richpolis@Mac-Richpolis.local>
2014-10-15 00:39:24 -0500

    Inicio del diseño de login

--- src/Richpolis/FrontendBundle/Controller/DefaultController.php
+++ src/Richpolis/FrontendBundle/Controller/DefaultController.php
@@ -5,12 +5,12 @@ namespace Richpolis\FrontendBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\HttpFoundation\Request;
 
 
 class DefaultController extends Controller
 {
+	
     /**
      * @Route("/",name="homepage")
      * @Template()
@@ -19,53 +19,14 @@ class DefaultController extends Controller
     {
         return array();
     }
-	
+
     /**
-     * @Route("/login", name="login")
+     * @Route("/registro",name="registro")
      * @Template()
      */
-    
-    public function loginAction()
+    public function registroAction(Request $request)
     {
-        $request = $this->getRequest();
-        $session = $request->getSession();
-        
-        $session->set('redirigir', $request->query->get('return',$this->generateUrl('homepage')));
-
-        // obtiene el error de inicio de sesión si lo hay
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(
-                SecurityContext::AUTHENTICATION_ERROR
-            );
-        } else {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        }
-
-        return $this->render(
-            'FrontendBundle:Default:login.html.twig',
-            array(
-                // último nombre de usuario ingresado
-                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-                'error'         => $error,
-            )
-        );
-    }
-	
-    /**
-     * @Route("/login_check", name="login_check")
-     */
-    public function securityCheckAction()
-    {
-        // The security layer will intercept this request
-    }
-
-    /**
-     * @Route("/logout", name="logout")
-     */
-    public function logoutAction()
-    {
-        // The security layer will intercept this request
-    }
+        return array();
+    }    
     
 }
